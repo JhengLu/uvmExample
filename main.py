4d4a9ca44ea7fa02507991ba9aca22090ffdc611
Jiaheng Lu <77672985+JhengLu@users.noreply.github.com>
2023-12-14 20:57:33 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ import torchvision
 import torchvision.transforms as transforms
 import torch.optim as optim
 from tqdm import tqdm
-import torch.cuda.amp as amp  # For mixed precision training
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Your Hyperparameters
+# Hyperparameters
 input_size = 784
 hidden_size = 50000
 num_classes = 1000
 num_epochs = 50
 batch_size = 500
 learning_rate = 0.001
+sub_batch_size = 100  # Sub-batch size for processing
 
 # MNIST dataset
 train_dataset = torchvision.datasets.MNIST(root='./data', train=True, transform=transforms.ToTensor(), download=True)
@@ -22,7 +22,6 @@ test_dataset = torchvision.datasets.MNIST(root='./data', train=False, transform=
 
 # Data loader
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
 
 # Model
 class LargerNN(nn.Module):
@@ -45,29 +44,34 @@ model = LargerNN(input_size, hidden_size, num_classes).to(device)
 loss_function = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-# Automatic Mixed Precision
-scaler = amp.GradScaler()
-
 # Training Process
-total_step = len(train_loader)
 for epoch in range(num_epochs):
     pbar = tqdm(train_loader)
     for i, (images, labels) in enumerate(pbar):
-        images = images.reshape(-1, 28*28).to(device)
-        labels = labels.to(device)
-        
-        optimizer.zero_grad()
+        images = images.reshape(-1, 28*28)
+        labels = labels
 
-        # Forward pass
-        with amp.autocast():  # Mixed precision
-            outputs = model(images)
-            loss = loss_function(outputs, labels)
-        
-        # Backward and optimize
-        scaler.scale(loss).backward()
-        scaler.step(optimizer)
-        scaler.update()
-        
-        pbar.set_description("Epoch [{}/{}], Loss: {:.4f}".format(epoch+1, num_epochs, loss.item()))
+        # Split batch into sub-batches
+        sub_batch_losses = []
+        for j in range(0, len(images), sub_batch_size):
+            sub_images = images[j:j+sub_batch_size].to(device)
+            sub_labels = labels[j:j+sub_batch_size].to(device)
+
+            # Forward pass
+            outputs = model(sub_images)
+            loss = loss_function(outputs, sub_labels)
+            sub_batch_losses.append(loss)
+
+            # Backward pass for sub-batch
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # Move the data back to CPU to free up GPU memory
+            sub_images.cpu()
+            sub_labels.cpu()
+
+        avg_loss = sum(sub_batch_losses) / len(sub_batch_losses)
+        pbar.set_description("Epoch [{}/{}], Loss: {:.4f}".format(epoch+1, num_epochs, avg_loss.item()))
 
 torch.save(model.state_dict(), 'model.ckpt')
